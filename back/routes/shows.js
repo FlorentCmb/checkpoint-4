7f7baad4a901831d954abffbc223164bcbe3c3fd
FlorentCmb <florent.cmb77@gmail.com>
2020-01-30 09:39:07 +0100

    Change tables and query methods

--- back/routes/shows.js
+++ back/routes/shows.js
@@ -20,41 +20,17 @@ router.get('/', (req, res) => {
 // Add a show
 router.post('/add', (req, res) => {
     const data = req.body
+    
     // Remove tickets data from the req.body ?
-    const showData = data.split(5, 1)
-    const ticketData = data[5]
-    connect.query('INSERT INTO shows SET ?', showData, (err, results) => {
+    console.log(data)
+    connect.query('INSERT INTO shows SET ?', data, (err, results) => {
         if (err) {
             res.sendStatus(500)
         }
         else {
-            // Add tickets query here
-            for (let i = 0; i < data.nb_places; i++) {
-                connect.query(`INSERT INTO tickets SET (price, id_show, id_user) VALUES (?, LAST_INSERT_ID(), NULL)`, ticketData, (err2, results2) => {
-                    if (err2) {
-                        res.sendStatus(500)
-                    }
-                    else {
-                        res.status(200).send('Show and its tickets has been added.')
-                    }
-                })
-            }
+            res.status(200).send('Show successfully added.')
         }
     })
 })
-// Update a show
-router.put('/:id/modify', (req, res) => {
-    const data = req.body
-    const id = req.params.id
-    connect.query('UPDATE shows SET ? WHERE shows.id=?', [data, id], (err, results) => {
-        if (err) {
-            res.sendStatus(500)
-        }
-        else {
-            res.status(200).send(`The show's changes (id: ${id}) has been successfully applied.`)
-        }
-    })
-})
-
 
 module.exports = router
